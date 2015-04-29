62297b8bf584b672378a97cde6e6b32d9f31cf02
Stas A. Kraev <mail@kraev.me>
2015-04-29 20:17:50 +1200

    Remove some private details

--- config.sh
+++ config.sh
@@ -1,2 +1,2 @@
-QUEUE="https://ap-southeast-2.queue.amazonaws.com/249594941299/api-batch"
-DY_TABLE="blix-infrastructure"
+QUEUE="https://ap-southeast-2.queue.amazonaws.com/<account-id>/api-batch"
+DY_TABLE="<dynamo-table>"
